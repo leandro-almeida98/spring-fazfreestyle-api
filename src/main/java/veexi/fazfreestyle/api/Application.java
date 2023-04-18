3c372aa2c1bd5d0087af8b04813e1f5f4d504a01
Leandro Almeida <leandro.almeida58@enova.educacao.com.br>
2023-04-17 23:52:53 -0300

    structure project

--- src/main/java/veexi/fazfreestyle/api/Application.java
+++ src/main/java/veexi/fazfreestyle/api/Application.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package veexi.fazfreestyle.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FazfreestyleApiApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FazfreestyleApiApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
